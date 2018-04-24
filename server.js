83dbdfd6c7fbcf1ba5c09a1f253bf357eb59916d
Sharma <661784@cognizant.com>
2018-04-24 11:14:47 +0530

    new files

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
-var http = require("http");
-http.createServer(function(request, response){
+var sfdx = require('sfdx-node');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Promise = require('promise');
+var path = require('path');
 
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	
-	response.end('Hello world\n');
-}).listen();
+const PORT = process.env.PORT || 3000;
 
-console.log('Server running');
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) { 
+
+	 console.log('Client connected...');
+	  
+      socket.on('AuthMyOrg', function()
+      {
+              sfdx.auth.webLogin({  
+          })
+          .then(function(Authmyorg){
+              console.log('Authorized org');
+              io.emit('Authmyorg',Authmyorg);
+          });
+      });
+
+
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
